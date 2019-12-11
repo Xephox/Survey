7ace674a735a925dffdd174b8d1544af29d30cb8
Xephox <58748080+Xephox@users.noreply.github.com>
2019-12-11 12:25:48 +0000

    Made ticket submission form.
    Created the form to submit a ticket number and set up questionaire page.

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 </head>
 <body>
     <h1>Survey</h1>
+    <p>Please select a ticket number and proceed to the questionaire.</p>
+    <form method="get" action="questionaire.php">
+        <input type="number" name="ticket" min="1" max="99">
+        <input type="submit" name="Submit">  
+    </form> 
 </body>
---
+++ questionaire.php
@@ -0,0 +1,1 @@
+<h1>Questionaire</h1>
