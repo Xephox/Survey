bbd148302a93ba8e0e2df21c1743b295333bc397
Xephox <58748080+Xephox@users.noreply.github.com>
2019-12-30 09:39:41 +0000

    Added admin page

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 </head>
 <body>
     <h1>Survey</h1>
+    <form method="post" action="admin.php">
+        <input type="username" name="username">
     <p>Please select a ticket number and proceed to the questionaire.</p>
-    <form method="get" action="questionaire.php">
+    <form method="post" action="questionaire.php">
         <input type="number" name="ticket" min="1" max="99">
         <input type="submit" name="Submit">  
     </form> 
